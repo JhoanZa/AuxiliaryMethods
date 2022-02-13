e17f7569d1bd94da997deb1d388aafd39a6aa77f
jhoan zambrano osorio <alejo.ax1306@gmail.com>
2022-02-13 00:58:52 -0500

    initial commit

---
+++ AuxiliaryMethods/SettingsMethods.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AuxiliaryMethods
+{
+    /// <summary>
+    ///    this class provides access methods for configuration files
+    /// </summary> 
+    internal class SettingsMethods
+    {
+        /// <summary>
+        ///     This method return the values content in the file of configuration .Json
+        /// </summary>
+        /// <param name="section">section name</param>
+        /// <param name="setting">setting name</param>
+        /// <param name="fileJson">file name .json</param>
+        /// <returns>configuration parameter content</returns>
+        public static string GetSettingsJson(string section, string setting, string fileJson = "appsettings.json")
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(fileJson, optional: true, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+
+            return configuration.GetSection(section).GetValue(setting, "0");
+        }
+
+    }
+}
